1a8e19c8ea049ec3e8f0dade783d1f9e452cb3f3
hajerhassan <Hajerhassanmohamed@gmail.com>
2023-08-10 12:45:34 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry proint
+ *
+ * Description:  A C programming that prints with printf
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
