44de6de6eace09d21855cbfa3c435ab4d2e25dcb
hajerhassan <Hajerhassanmohamed@gmail.com>
2023-08-13 10:17:08 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: the if statement program
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+int ch = 97;
+while (ch <= 122)
+{
+putchar(ch);
+ch++;
+}
+putchar("\n");
+return (0);
+}
