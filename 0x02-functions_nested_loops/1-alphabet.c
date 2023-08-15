241075a7c4b6bd44f9e23403736e11300e282d53
hajerhassan <Hajerhassanmohamed@gmail.com>
2023-08-15 00:49:53 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet from a - z
+*/
+
+void print_alphabet(void)
+{ 
+int n = 97;
+while (n <= 122)
+{
+_putchar(n);
+n++;
+}
+_putchar('\n');
+return (0);
+}
