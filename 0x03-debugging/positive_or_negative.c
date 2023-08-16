904d7944a230ea2a79b210fb8a4a6f782f686593
hajerhassan <Hajerhassanmohamed@gmail.com>
2023-08-16 12:17:57 -0700

    update

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -18,7 +18,7 @@ void positive_or_negative(int i)
 srand(time(0));
 if (i > 0)
 printf("%d is positive\n", i);
-else  if(i<0)
+else if (i < 0)
 printf("%d is negative\n", i);
 else
 printf("%d is zero\n", i);
