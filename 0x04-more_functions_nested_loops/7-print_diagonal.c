8863a39306729a6136953551e8fa361a4e316c90
hajerhassan <Hajerhassanmohamed@gmail.com>
2023-08-17 12:19:12 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,22 +5,22 @@
  *
  * @n: is the number of time the \ character
  *          should be printed
-*/
+ */
 
 void print_diagonal(int n)
 {
-int slash, space;
+	int slash, space;
 
-if (n <= 0)
-_putchar('\n');
-else
-{
-	for (slash = 1; slash <= n; slash++)
-{
-	for (space = 1; space <= slash; space++)
-_putchar(' ');
-_putchar(92);
-_putchar('\n');
-}
-}
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (slash = 1; slash <= n; slash++)
+		{
+			for (space = 1; space <= slash; space++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
 }
