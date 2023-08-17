3ffb2dbf028be3839547b9a3a877517f25ff4faf
hajerhassan <Hajerhassanmohamed@gmail.com>
2023-08-17 15:31:15 -0700

    update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,16 +5,20 @@
  *      from 0 to 9 except 2 and 4
  *
  * Return: always 0 success
-*/
+ */
 
 void print_most_numbers(void)
 {
-int ch;
-for (ch = 0; ch <= 9; ch++)
-{
-if (ch == 2 || ch == 4)
-continue;
-_putchar(ch + 48);
-}
-_putchar('\n');
+	int ch;
+	for (ch = 48; ch <= 57; ch++)
+	{
+		if (ch != 50)
+		{
+			if (ch != 52)
+			{
+				_putchar(ch);
+			}
+		}
+	}
+	_putchar('\n');
 }
