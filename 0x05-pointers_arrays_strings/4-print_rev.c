54859703c0a75c8f1442dc26582b79c9460372c2
hajerhassan <Hajerhassanmohamed@gmail.com>
2023-08-21 03:06:04 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - function that print in
+ * reserve
+ *
+ * @s: pointer that point for variabe
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	for (*s = '\0'; *s > [0]; s--)
+	{
+		_putchar(s + 0);
+	}
+	_putchar('\n');
+}
