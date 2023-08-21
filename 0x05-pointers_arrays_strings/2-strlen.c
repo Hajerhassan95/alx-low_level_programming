4c375a782e470318b894f0a71e59f0357c4f37f4
hajerhassan <Hajerhassanmohamed@gmail.com>
2023-08-21 02:25:49 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - function that print the length of
+ * string
+ *
+ * @s: pointer that point to variable
+ *
+ * Return: always 0 success
+*/
+
+int _strlen(char *s)
+{
+strlen(*s);
+}
