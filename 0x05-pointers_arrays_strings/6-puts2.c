f090b845ddc5a695047fcc9a27a4abb3af4d490b
hajerhassan <Hajerhassanmohamed@gmail.com>
2023-08-21 07:20:01 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts2 - function that print one character
+ *
+ * @str: pointer that point to the variable
+ *
+ * return: print
+ */
+
+void puts2(char *str)
+{
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0; o <= t; o++)
+	{
+		if (o % 2 == 0)
+		{
+			_putchar(str[o]);
+		}
+	}
+
+	-putchar('\n');
+}
